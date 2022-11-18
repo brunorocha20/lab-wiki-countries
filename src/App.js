44cc6bb50cb3f2e480d474e145502a3a76c7012a
Bruno Rocha <bruno.miguel.rocha@hotmail.com>
2022-11-18 00:42:55 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [fetching, setFetching] = useState(true);
+
+  const getFromApi = async () => {
+    try {
+      const response = await axios.get(
+        `https://ih-countries-api.herokuapp.com/countries`
+      );
+      setCountries(response.data);
+      setFetching(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFromApi();
+  }, []);
+  console.log(countries);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            fetching ? (
+              <h1>Please wait</h1>
+            ) : (
+              <CountriesList countries={countries} />
+            )
+          }
+        />
+        <Route
+          path="/country/:id"
+          element={
+            fetching ? (
+              <h1>I said WAIT!</h1>
+            ) : (
+              <CountryDetails countries={countries} />
+            )
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+            <NavLink to="/" className='navbar-brand'>
+            WikiCountries
+            </NavLink>
+        </div>
+    </nav>
+  );
+}
+
+export default Navbar;
